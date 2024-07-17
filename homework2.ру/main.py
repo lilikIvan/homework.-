1ae146298ca1d2e37cd1fab91d063bb74ca0d842
lilikIvan <lilik_2002@mail.ru>
2024-07-17 15:52:41 +0300

    Add files via upload

---
+++ homework2.ру/main.py
@@ -0,0 +1,9 @@
+friend_name = "Kirill"
+money_count = "300 dollars"
+anotherFrienName = "Alexey"
+moneyCount = "300 dollars"
+cost_car = '600 dolars'
+name = "car"
+name_car = "car,"
+a = "works"
+print (friend_name, "и", anotherFrienName, "скинулись на", name, 'со стоимостью в', cost_car, "по", money_count, "=", moneyCount, "для покупки новой", name_car, "для совместной", a)

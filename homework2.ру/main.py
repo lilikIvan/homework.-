c1af5492d1e9fa6e305c9a36b350e424b9073d51
lilikIvan <lilik_2002@mail.ru>
2024-07-18 23:47:29 +0300

    Add files via upload

--- homework2.ру/main.py
+++ homework2.ру/main.py
@@ -1,7 +1,5 @@
-course_name = 'Python,'
-The_number_of_household_chores_completed = '12,'
-NumberOfHoursSpent = '1.5,'
-a = 12
-b = 1.5
-TimeForOneTask = b/a
-print(course_name, 'всего задач:', The_number_of_household_chores_completed, 'затрачено часов:', NumberOfHoursSpent, 'среднее время выполнения', TimeForOneTask)
+course_name = 'Python'','
+The_number_of_household_chores_completed = 12
+NumberOfHoursSpent = 1.5
+TimeForOneTask = NumberOfHoursSpent/The_number_of_household_chores_completed
+print(course_name, 'всего задач:', str(The_number_of_household_chores_completed)+',', 'затрачено часов:', str(NumberOfHoursSpent)+',', 'среднее время выполнения', str(TimeForOneTask)+'.')

9d080658c5e280aa7050aa83035a5f177eb06865
lilikIvan <lilik_2002@mail.ru>
2024-07-31 22:06:21 +0300

    Add files via upload

---
+++ module_1_6.py/main.py
@@ -0,0 +1,15 @@
+my_dict = {'Ivan': 2002}
+print(my_dict)
+print(my_dict.get('Ivan'), my_dict.get('Karina'))
+my_dict ['Kirill'] = 2003
+my_dict ['Anna'] = 1996
+del my_dict ['Ivan']
+print (my_dict)
+
+my_set = {1, 2, 3, 1, 2, 3,}
+print (my_set)
+my_set.add ('Orange')
+my_set.add (31.31412331)
+print(my_set)
+my_set.remove ('Orange') #Можно использовать '.discard'
+print (my_set)

abe0ee0e2c79a32192381580014eda2d17c478ae
lilikIvan <lilik_2002@mail.ru>
2024-08-07 15:59:29 +0300

    Add files via upload

---
+++ module_2_2.py/main.py
@@ -0,0 +1,19 @@
+first = 123
+second = 456
+third = 789
+if first == second == third:
+    print('3')
+elif first == second or second == third or first == third:
+    print ('2')
+else :
+    print ('0')
+
+first = 789
+second = 456
+third = 789
+if first == second == third:
+    print('3')
+elif first == second or second == third or first == third:
+    print ('2')
+else :
+    print ('0')

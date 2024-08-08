50ff6959657b1b501f162659d874486551d3e51f
lilikIvan <lilik_2002@mail.ru>
2024-08-08 15:04:20 +0300

    Add files via upload

--- module_2_3.py/main.py
+++ module_2_3.py/main.py
@@ -1,10 +1,14 @@
 myList = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 while index < len(myList):
-    if myList[index] == 9:
-        break
-
-    if myList[index] > 1:
+    if myList[index] > 0:
         print(myList[index])
-    index += 1
+        index += 1
+        continue
 
+    if myList[index] == 0:
+        index += 1
+        continue
+
+    if myList[index] < 0:
+        break
